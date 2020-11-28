257510da6924f6c9ad763326f4cb3666aaddff2c
Moises Barillas <moisesrocha717@gmail.com>
2020-11-27 18:21:13 -0600

    Obteniendo todos los usuarios de la base de datos

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -9,7 +9,22 @@ const Usuario = require("../models/usuario");
 const app = express();
 
 app.get("/usuario", function (req, res) {
-	res.json("get usuarios local");
+	let desde = req.query.desde || 0;
+	desde = Number(desde);
+
+	let limite = req.query.limite || 5;
+	limite = Number(limite);
+
+	Usuario.find({})
+		.skip(desde)
+		.limit(5)
+		.exec((err, usuarios) => {
+			if (err) {
+				return res.status(400).json({ ok: false, err });
+			}
+
+			res.json({ ok: true, usuarios });
+		});
 });
 
 app.post("/usuario", function (req, res) {
