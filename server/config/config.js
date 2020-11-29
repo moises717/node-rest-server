d6ffece032570a70b4fcacd35ea993226a65896a
Moises Barillas <moisesrocha717@gmail.com>
2020-11-28 21:07:31 -0600

    Varable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -17,8 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
 	urlDB = "mongodb://localhost:27017/cafe";
 } else {
-	urlDB =
-		"mongodb+srv://moises717:moises1998@restserver.qyshh.mongodb.net/<dbname>?retryWrites=true&w=majority";
+	urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
